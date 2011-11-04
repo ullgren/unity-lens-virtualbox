114ca1ec0afd7770fb080a19de76010118e4a2e2
stone <stone4x4@gmail.com>
2011-11-04 11:40:10 +0100

    Python setuptools, dbus config
    Added setup.py, MANIFEST.in for easy installation and integration
    with packaging/PyPi.
    
    Added dbus service to autostart daemon.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+from distutils.core import setup
+#from DistUtilsExtra.command import *
+import os
+
+setup(name="unity-lens-vbox",
+    version="0.1",
+    author="Pontus Ullgren, Fredrik Steen",
+    author_email="ullgren@gmail.com, stone4x4@gmail.com",
+    url="https://github.com/ullgren/unity-lens-virtualbox",
+    license="GNU General Public License (GPL)",
+    data_files=[
+        ('share/unity/lenses/vbox', ['vbox.lens']),
+        ('share/dbus-1/services', ['unity-lens-vbox.service']),
+        ('lib/unity-lens-vbox', ['unity-lens-vbox.py']),
+        ('share/unity-lens-vbox', ['virtualbox.svg']),
+    ])
+#cmdclass={"build":  build_extra.build_extra, })
+
+os.system('setsid unity')
